71f10639ff4cab2e5e748c696a8f6a99c26dbb6b
Ashutosh <ashutosh.singh3889@gmail.com>
2019-05-23 19:52:00 +0530

    fresh

---
+++ src/containers/Container.js
@@ -0,0 +1,31 @@
+		import React ,{Component} from 'react';		
+		import { connect } from "react-redux";
+		//import CardDeckBody from './CardDeckBody.js';
+		//import DrawnCardDeck from './DrawnCardDeck.js';		
+		import Kitelogin from './KiteLogin.js';			
+		import LoginNav from './LoginNav.js';
+		import TestLoginNav from './testLoginNav.js';
+		import TestBLoginNav from './b_testLoginNav.js';
+		import DojiLogin from './dojiLogin.js';
+        import { Switch, Route } from 'react-router-dom';
+
+
+
+		export default  class Board extends Component{
+
+		  render(){
+
+		    return(
+		       <div>
+		        <Switch>
+                     <Route  path='/login' component={Kitelogin}/>                     
+                     <Route exact path='/success/' component={LoginNav}/>
+                     <Route exact path='/test' component={TestLoginNav}/>
+                     <Route exact path='/doji' component={DojiLogin}/>
+                     <Route exact path='/btest' component={TestBLoginNav}/>
+
+                </Switch>		 
+		        </div>)
+		    }
+		}
+
